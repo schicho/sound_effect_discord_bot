5c8bcb888e8c9877bf096e9526ad5f84c1072fb5
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2020-10-29 22:44:39 +0100

    add discord presence

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import discord
 from discord import LoginFailure
 from discord.ext import commands
 
 import soundeffectCog
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!', description='hit me up with !se')
 token = open("token.txt", "r").readline()
 
 
@@ -16,6 +17,8 @@ async def ping(ctx):
 async def on_ready():
     print('ONLINE! {0} ({0.id})'.format(bot.user))
     print('------')
+    await bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.playing, name="Horrible Sound Effects"))
 
 
 if __name__ == "__main__":
